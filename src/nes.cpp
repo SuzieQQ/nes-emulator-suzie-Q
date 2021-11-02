56eb02cb6525125984949c365ec17edbd09acb03
SuzieQQ <44621050+SuzieQQ@users.noreply.github.com>
2021-11-02 01:52:27 +0100

    Add files via upload

---
+++ src/nes.cpp
@@ -0,0 +1,9 @@
+#include<fstream>
+#include<vector>
+#include<iostream>
+#include "nes.h"
+
+/*
+std::ifstream f{"Zelda.nes", std::ios::binary};
+std::vector<T> rom{std::istreambuf_iterator<char>{f}, {}};
+*/
