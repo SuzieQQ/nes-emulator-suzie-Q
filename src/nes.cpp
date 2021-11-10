55cf1cddef205453b56b50ff3b38c855a87863ce
SuzieQ <44621050+SuzieQQ@users.noreply.github.com>
2021-11-10 21:21:39 +0100

    Delete src directory

--- src/nes.cpp
+++
@@ -1,9 +0,0 @@
-#include<fstream>
-#include<vector>
-#include<iostream>
-#include "nes.h"
-
-/*
-std::ifstream f{"Zelda.nes", std::ios::binary};
-std::vector<T> rom{std::istreambuf_iterator<char>{f}, {}};
-*/
