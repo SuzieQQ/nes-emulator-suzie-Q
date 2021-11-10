a2786bc2c8affb9a8a0c3591ab4d80cb357f9ccb
SuzieQ <44621050+SuzieQQ@users.noreply.github.com>
2021-11-10 22:16:46 +0100

    Add files via upload

--- src/Cpu.h
+++ src/Cpu.h
@@ -35,39 +35,35 @@ public:
 
 
 
-  
-  //opcode
-u8 ADC();   u8	AND(); 	u8 ASL(); 	u8 BCC(); 	
 
-u8 BCS(); 	u8 BEQ(); 	u8 BIT(); 	u8 BMI(); 	
+//opcode
+u8 ADC();      u8 AND(); 	u8 ASL(); 	u8 BCC(); 	
 
-u8 BNE(); 	u8 BPL(); 	u8 BRK(); 	u8 BVC(); 	
+u8 BCS();      u8 BEQ(); 	u8 BIT(); 	u8 BMI(); 	
 
-u8 BVS(); 	u8 CLC();   u8 CLD();   u8 CLI();
+u8 BNE();      u8 BPL(); 	u8 BRK(); 	u8 BVC(); 	
 
-u8 CLV(); 	u8 CMP(); 	u8 CPX();   u8 CPY();
+u8 BVS();      u8 CLC();       u8 CLD();      u8 CLI();
 
-u8 DEC(); 	u8 DEX();   u8 DEY(); 	u8 EOR();
+u8 CLV();      u8 CMP(); 	u8 CPX();      u8 CPY();
 
-u8 INC(); 	u8 INX();   u8 INY(); 	u8 JMP();
+u8 DEC();      u8 DEX();       u8 DEY();      u8 EOR();
 
-u8 JSR();   u8 LDA(); 	u8 LDX();   u8 LDY();
+u8 INC();      u8 INX();       u8 INY();      u8 JMP();
 
-u8 LSR(); 	u8 NOP(); 	u8 ORA();   u8 PHA();
+u8 JSR();      u8 LDA(); 	u8 LDX();      u8 LDY();
 
-u8 PHP(); 	u8 PLA(); 	u8 PLP();   u8 ROL(); 	
+u8 LSR();      u8 NOP(); 	u8 ORA();      u8 PHA();
 
-u8 RTS();   u8	SBC();  u8 SEC();   u8 SED();
+u8 PHP();      u8 PLA(); 	u8 PLP();      u8 ROL(); 	
+
+u8 RTS();      u8  SBC();      u8 SEC();      u8 SED();
  	
-u8 SEI(); 	u8 STA(); 	u8 STX();   u8 STY();
+u8 SEI();      u8 STA(); 	u8 STX();      u8 STY();
 
-u8 TAX(); 	u8 TAY(); 	u8 TSX();   u8 TXA();
+u8 TAX();      u8 TAY(); 	u8 TSX();      u8 TXA();
 
-u8 TXS(); 	u8 TYA();  u8 ROR();   u8 RTI();
-  
-  
-
-
+u8 TXS();      u8 TYA();       u8 ROR();      u8 RTI();
 
 
 //Addr
@@ -115,7 +111,7 @@ u8 PC;       //Program_Counter
 u8 SP;      //Stack_Pointer
 u8 ST_REG; //Status_Register
 u8 m_skipCycles;
-u8 m_cycles; 
+u8 m_cycles;
 
 
 
